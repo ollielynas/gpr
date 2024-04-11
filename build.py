666d57ebf491ebefa95c1e6d54bcba3fa23b8341
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-04-11 23:13:21 +1200

    fixed link

--- build.py
+++ build.py
@@ -10,13 +10,26 @@ with open("template.html", "r") as my_file:
     html_template = my_file.read()
 
 
+game_links = ""
+
 
 for i in games:
     html = html_template
     
     html = html.replace("TITLE", i)
-    html = html.replace("NAME", games[i][0])
+    html = html.replace("NAME", games[i][1])
     
     
     with open(f"games/{i}.html", "w") as my_file:
         my_file.write(html)
+
+    if i != "error":
+        game_links += f"<a href='https://ollielynas.github.io/gpr/games/{i}.html?'>a<img src='https://ollielynas.github.io/gpr/img/{i}.png' alt='start game of {games[i][1]}'></img></a>"
+
+index = ""
+with open("index.html", "r") as my_file:
+    index = my_file.read().split("<!-- GAMES -->")
+
+with open("index.html", "w") as my_file:
+    index[1] = game_links
+    index = my_file.write("<!-- GAMES -->".join(index))
